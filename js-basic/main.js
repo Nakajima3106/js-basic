69b024bc36a994bc14af6888975e00c13740c5be
Nakajima3106 <uver777.333@gmail.com>
2021-03-04 04:13:37 +0000

    5章終了

--- js-basic/main.js
+++ js-basic/main.js
@@ -1,5 +1,7 @@
-var obj = {
-  text: 'Javascriptを勉強中です。',
-  days: 5,
-};
-console.log("私は" + obj.days + "日間" + obj.text);
+var h1 = document.getElementsByTagName('h1');
+h1[0].addEventListener('click', function () {
+  h1[0].innerHTML = 'はじめてのDOM操作';
+  var p = document.createElement('p');
+  p.innerHTML = 'Javascriptって面白い！';
+  h1[0].after(p);
+});
